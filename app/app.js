b4539d7f0920b36f0631b945ec7f55cf67bc616a
John Ghatas <johnghatas97@hotmail.com>
2020-02-25 11:44:29 +0100

    Fixed the express server not closing

--- app/app.js
+++ app/app.js
@@ -1,43 +1,47 @@
-import express from 'express'
-import mariadb from 'mariadb'
-import dotenv from 'dotenv'
-import routes from './routes'
+import express from "express";
+import mariadb from "mariadb";
+import dotenv from "dotenv";
+import routes from "./routes";
 
 // Initialize
-const PORT = 4000
-const server = express()
-dotenv.config()
+const PORT = 4000;
+const server = express();
+dotenv.config();
 
 // Connecting to the SQL database
 const database = mariadb.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: 'sfs',
-    connectTimeout: 30000
-})
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: "sfs",
+  connectTimeout: 30000
+});
 
-database.getConnection()
-    .then(() => {
-        console.log(`Successfully connected to MySQL database at ${process.env.DB_HOST}:${process.env.DB_PORT}.`)
-    })
-    .catch(err => {
-        console.error(err)
-        process.exit()
-    })
+database
+  .getConnection()
+  .then(() => {
+    console.log(
+      `Successfully connected to MySQL database at ${process.env.DB_HOST}:${process.env.DB_PORT}.`
+    );
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit();
+  });
 
 // Getting all routes and applying them
 Object.entries(routes).forEach(([key, value]) => {
-    server.use(key, (req, res) => value(req, res, database))
-})
+  server.use(key, (req, res) => value(req, res, database));
+});
 
 // Running the server
-server.listen(PORT, server => {
-    console.log(`Backend is running on port ${PORT}.`)
-})
+server.listen(PORT, () => {
+  console.log(`Backend is running on port ${PORT}.`);
+});
 
 // Allow graceful closure of database connection
-process.on('SIGINT', () => {
-    database.end()
-})
+process.on("SIGINT", () => {
+  database.end();
+  process.exit();
+});
