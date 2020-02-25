dc528b7d542969f36162ba81f468a121c393dd8d
John Ghatas <johnghatas97@hotmail.com>
2020-02-25 10:42:26 +0100

    Finished script

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd ..
-dir=${PWD##*/}  
 docker-compose build
-docker run -p 4000:4000 -d "$dir_app"
-docker run -p 3000:3000 -d "$dir_client"
+docker run -p 4000:4000 -d "sfs-hackaton_app"
+docker run -p 3000:3000 -d "sfs-hackaton_client"
