371a84382b5f805d46f943e9f551a724a321a70b
John Ghatas <johnghatas97@hotmail.com>
2020-02-25 10:29:47 +0100

    Adjusted script to work across all machiens

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd ..
+dir=${PWD##*/}  
 docker-compose build
-docker run -p 4000:4000 -d hackaton_app
-docker run -p 3000:3000 -d hackaton_client
+docker run -p 4000:4000 -d "$dir_app"
+docker run -p 3000:3000 -d "$dir_client"
