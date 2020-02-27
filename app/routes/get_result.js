fe95cfb0c118dd8cd8f90cd91626de6f9eeb6dcb
MauritsioRK <maurits_rk@hotmail.com>
2020-02-27 13:54:28 +0100

    Add endpoint for getting user results with minor descriptions and links

--- app/routes/get_result.js
+++ app/routes/get_result.js
@@ -1,17 +1,39 @@
 const method = "GET";
 
+const baseUrl = "https://minoren.mijnhva.nl/";
+
 const call = (request, response, database) => {
-  const { id } = request.query;
+  const { id, lang } = request.query;
 
   database
     .query(`SELECT programme, scores FROM Results WHERE id = '${id}'`)
     .then(rows => {
-      rows[0].scores = JSON.parse(rows[0].scores);
-      rows[0].scores.forEach(row => {
-        row.similarity = parseFloat(row.similarity);
-      });
+      const results = rows[0];
+      results.scores = JSON.parse(results.scores);
 
-      response.json(rows[0]);
+      const minorsQuery = results.scores
+        .map(score => `name = '${score.name}'`)
+        .join(" OR ");
+
+      database
+        .query(
+          `SELECT description_${lang} AS description FROM Minors WHERE ${minorsQuery}`
+        )
+        .then(minors => {
+          results.scores.forEach((score, index) => {
+            score.similarity = parseFloat(score.similarity);
+            score.description = minors[index].description;
+            const snakeCasedName = score.name.toLowerCase().replace(/ /g, "-");
+            score.link = `${baseUrl}${
+              lang === "EN" ? "en/" : ""
+            }2019-2020/${snakeCasedName}`;
+          });
+
+          response.json(results);
+        })
+        .catch(error => {
+          response.json(error);
+        });
     })
     .catch(error => {
       response.json(error);
